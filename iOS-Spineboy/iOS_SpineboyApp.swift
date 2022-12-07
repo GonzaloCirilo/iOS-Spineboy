008df2351903be583660d27cb14155265ec3c68b
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2022-12-06 20:43:04 -0500

    Initial Commit

---
+++ iOS-Spineboy/iOS_SpineboyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SpineboyApp.swift
+//  iOS-Spineboy
+//
+//  Created by Gonzalo Cirilo Herrera on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SpineboyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
