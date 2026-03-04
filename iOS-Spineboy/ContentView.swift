e5af3cfb1baa89bd8f6b28669bf37d6b2fc7de50
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2026-03-04 01:35:17 -0500

    Migrate to official spine-ios SDK

--- iOS-Spineboy/ContentView.swift
+++ iOS-Spineboy/ContentView.swift
@@ -6,21 +6,26 @@
 //
 
 import SwiftUI
+import Spine
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    @StateObject var controller = SpineController(
+        onInitialized: { controller in
+            controller.animationState.setAnimationByName(trackIndex: 0, animationName: "walk", loop: true)
+        }
+    )
+
+    var body: some View {
+        SpineView(
+            from: .bundle(
+                atlasFileName: "spineboy-pma.atlas",
+                skeletonFileName: "spineboy-pro.skel"
+            ),
+            controller: controller,
+            mode: .fit,
+            alignment: .center
+        )
+        .ignoresSafeArea()
     }
 }
---
+++ iOS-Spineboy/iOS-Spineboy-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  iOS-Spineboy-Bridging-Header.h
+//  iOS-Spineboy
+//
+//  Created by Gonzalo Cirilo Herrera on 26/01/23.
+//
+
+#ifndef iOS_Spineboy_Bridging_Header_h
+#define iOS_Spineboy_Bridging_Header_h
+
+
+
+#endif /* iOS_Spineboy_Bridging_Header_h */
